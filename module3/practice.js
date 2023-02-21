dce0bb9a3d40356e67aab75aa51067015fefd9a2
ambermhh <ambermhh@hotmail.com>
2023-02-21 17:07:05 +1100

    123

--- module3/practice.js
+++ module3/practice.js
@@ -1,1 +1,51 @@
-console.log("hello world")
+// /* "" + 1 + 0  //string at the beginning 
+// "" - 1 + 0  //string at the beginning
+// true + false //true is 1, false is 0, 1+0=1
+// !true //false ~
+// 6 / "3" //2~
+// "2" * "3" //6~
+// 4 + 5 + "px" //9px~
+// "$" + 4 + 5 //$45~
+// "4" - 2 //2~
+// " -9 " + 5 // -9 5~
+// " -9 " - 5 //-14~
+// null + 1 //1~
+// undefined + 1 //undefined x  (NaN)
+// undefined == null //true
+// undefined === null //false
+// " \t \n" - 2 //
+
+// console.log(" \t \n" - 2)
+
+//  */
+
+// if (0) console.log('#1 zero is true')
+// if ("0") console.log('#2 zero is true')
+// if (null) console.log('null is true')
+// if (-1) console.log('negative is true')
+// if (1) console.log('positive is true')
+
+
+// let a = 10, b = 3;
+// let result = `${a} + ${b} is `;
+// if (a + b < 10 ? true:false) {
+// result += 'less than 10';
+// } else {
+// result += 'greater than 10';}
+
+// console.log(result)
+
+
+
+
+getGreeting =()=> {
+    return "Hello " +   "!"
+}
+
+console.log(getGreeting())
+
+
+
+
+
+
