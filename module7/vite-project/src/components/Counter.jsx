c2087594515625c598c8632e4a0794dec6e89121
ambermhh <ambermhh@hotmail.com>
2023-03-23 10:04:01 +1100

    123

---
+++ module7/vite-project/src/components/Counter.jsx
@@ -0,0 +1,22 @@
+
+import React, {useState, useEffect} from 'react'
+
+function Counter() {
+
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    }, [count])
+
+    return (
+        <div className="Counter componentBox">
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count+1)}>Click Me!</button>
+           
+            {/* how could we make a reset button to reset the count back to zero? */}
+        </div>
+    )
+}
+
+export default Counter
